534f984035c6f6a95d8af4d2e46dc4a06a7e6823
Владислав Бережнов <berezhnovvladislav2012@gmail.com>
2025-02-17 12:03:03 +0300

    Added no correct test method

--- src/test/java/ru/berezhnov/SpringcourseProject3ApplicationTests.java
+++ src/test/java/ru/berezhnov/SpringcourseProject3ApplicationTests.java
@@ -1,13 +1,29 @@
 package ru.berezhnov;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.client.RestTemplate;
+import ru.berezhnov.dto.MeasurementDTO;
+import ru.berezhnov.dto.SensorDTO;
 
 @SpringBootTest
-class SpringcourseProject3ApplicationTests {
+public class SpringcourseProject3ApplicationTests {
+
+	private final RestTemplate restTemplate;
+
+	@Autowired
+	public SpringcourseProject3ApplicationTests(RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
 
 	@Test
 	void contextLoads() {
+		for (int i = 0; i < 1000; i++) {
+			MeasurementDTO measurementDTO = new MeasurementDTO((Math.random() - 0.5) * 200,
+					Math.random() > 0.5, new SensorDTO("test sensor"));
+			restTemplate.postForObject("https://localhost:8080/measurements/add", measurementDTO, String.class);
+		}
 	}
 
 }
